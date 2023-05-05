7c0fc5a4809a7c06ad9a084abe4d7b0901a15f7a
Centy_GG <makarsyshchikov04@gmail.com>
2023-05-05 13:31:48 +0300

    модель + рум

---
+++ app/src/main/java/com/example/victorina20/FragmentQuestions.java
@@ -0,0 +1,28 @@
+package com.example.victorina20;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.victorina20.databinding.FragmentMainMenuBinding;
+
+public class FragmentQuestions extends Fragment {
+
+    private FragmentQuestions binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentQuestions.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
--- app/src/main/java/com/example/victorina20/Room.java
+++ app/src/main/java/com/example/victorina20/Room.java
@@ -1,4 +1,28 @@
 package com.example.victorina20;
 
-public class Room {
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.victorina20.databinding.ActivityRoomBinding;
+
+public class Room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
 }
